96c72e48fc5b620160dcdf8091a6bdaf36ecda21
Robin Wilkie <robinwilkieglasgow@hotmail.com>
2017-02-22 11:54:37 +0000

    started part3

--- src/app/providers/af.ts
+++ src/app/providers/af.ts
@@ -2,11 +2,18 @@
  * Created by dev on 22/02/17.
  */
 // src/app/providers/af.ts
-import {Injectable} from "@angular/core";
-import {AngularFire, AuthProviders, AuthMethods} from 'angularfire2';
+
+import {Injectable} from '@angular/core';
+import {AngularFire, AuthProviders, AuthMethods, FirebaseListObservable} from 'angularfire2';
 @Injectable()
 export class AF {
-  constructor(public af: AngularFire) {}
+  public messages: FirebaseListObservable<any>;
+  public users: FirebaseListObservable<any>;
+  public displayName: string;
+  public email: string;
+  constructor(public af: AngularFire) {
+    this.messages = this.af.database.list('messages');
+  }
   /**
    * Logs in the user
    * @returns {firebase.Promise<FirebaseAuthState>}
@@ -22,5 +29,18 @@ export class AF {
    */
   logout() {
     return this.af.auth.logout();
+  }
+  /**
+   * Saves a message to the Firebase Realtime Database
+   * @param text
+   */
+  sendMessage(text) {
+    var message = {
+      message: text,
+      displayName: this.displayName,
+      email: this.email,
+      timestamp: Date.now()
+    };
+    this.messages.push(message);
   }
 }
